2387b33d1f872edbef0699af144784d148472fdd
Dane Carr <dane.l.w.carr@gmail.com>
2012-01-12 15:01:11 -0500

    Removed some old commented code and unused variables

--- Twitter App/TwitterAppAppDelegate.m
+++ Twitter App/TwitterAppAppDelegate.m
@@ -10,61 +10,14 @@
 #import "TwitterAppViewController.h"
 #import "Tweet.h"
 
-@implementation TwitterAppAppDelegate {
-    NSMutableArray *tweets;
-}
+@implementation TwitterAppAppDelegate
 
 @synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-//    //create sample tweet and add to tweets array
-//    tweets = [NSMutableArray arrayWithCapacity:20];
-//    Tweet *tweet = [[Tweet alloc] init];
-//    
-//    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-//    [dateFormatter setDateFormat:@"eee, dd MMM yyyy HH:mm:ss ZZZZ"];
-//    
-//    tweet.createdAt = [dateFormatter dateFromString:@"Fri, 06 Jan 2012 22:30:17 +0000"];
-//    tweet.fromUser = @"reddit";
-//    tweet.fromUserIDString = @"811377";
-//    tweet.fromUserName = @"the reddit alien";
-//    tweet.tweetIDString = @"155415918216888320";
-//    tweet.profileImgURL = [NSURL URLWithString:@"http://a3.twimg.com/profile_images/275642219/reddit-twitter-transp_bigger_normal.png"];
-//    tweet.tweetText = @"These things happen sometimes. http://t.co/3286eRtf [fffffffuuuuuuuuuuuu] 1056 points, submitted by stabbingbrainiac [http://t.co/LNzouCiE]";
-//    tweet.toUser = nil;
-//    tweet.toUserIDStr = nil;
-//    tweet.toUserName = nil;
-//    
-//    [tweets addObject:tweet];
-//    
-//    tweet = [[Tweet alloc] init];
-//    tweet.createdAt = [dateFormatter dateFromString:@"Mon, 09 Jan 2012 19:33:13 +0000"];
-//    tweet.fromUser = @"reddit";
-//    tweet.fromUserIDString = @"811377";
-//    tweet.fromUserName = @"the reddit alien";
-//    tweet.tweetIDString = @"155415918216888320";
-//    tweet.profileImgURL = [NSURL URLWithString:@"http://a3.twimg.com/profile_images/275642219/reddit-twitter-transp_bigger_normal.png"];
-//    tweet.tweetText = @"REPORT: News Networks Ignore Controversial SOPA Legislation http://t.co/3k0f0FsH";
-//    tweet.toUser = nil;
-//    tweet.toUserIDStr = nil;
-//    tweet.toUserName = nil;
-//    
-//    [tweets addObject:tweet];
-    
-//    NSURL *searchURL = [[NSURL alloc] initWithString:@"http://search.twitter.com/search.json?q=twg&rpp=20&include_entities=1"];
-//    
-//    __block NSMutableArray *tweets = [[NSMutableArray alloc] init];
-//    
-//    NSBlockOperation *getTweets = [NSBlockOperation blockOperationWithBlock: ^(void){
-//        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:searchURL];
-//        [[NSURLConnection alloc] initWithRequest:request delegate:self];
-//        
-//    }];
-//    
-//    TwitterAppViewController *twitterAppViewController = (TwitterAppViewController *) self.window.rootViewController;
-//    twitterAppViewController.tweets = tweets;
+
     
     return YES;
 }
